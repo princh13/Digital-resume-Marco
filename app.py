e4de8629083601bd8b02cdf7c31bcad05e26cc3a
Marco Principe <marcoprincipe1013@gmail.com>
2023-04-28 14:05:17 -0400

    changes to app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 # css_file = current_dir / "styles" / "main.css"
-css_file = r"./styles/main.css"
+css_file = r".\styles\main.css"
 # resume_file = current_dir / "assets" / "CV.pdf"
-resume_file = r"./assets/CV.pdf"
+resume_file = r".\assets\CV.pdf"
 # profile_pic = current_dir / "assets" / "profile-pic.png"
-profile_pic = r"./assets/profile-pic.png"
+profile_pic = r".\assets\profile-pic.png"
 
 
 # --- GENERAL SETTINGS ---
